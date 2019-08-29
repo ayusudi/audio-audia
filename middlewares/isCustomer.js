884697f0a4cfa1ca7a024da153c4a9cbb93ffd00
Ayu Sudi <ayusudi.abc@gmail.com>
2019-08-29 16:10:07 +0700

    route

--- middlewares/isCustomer.js
+++ middlewares/isCustomer.js
@@ -1,7 +1,14 @@
 module.exports = (req, res, next) => {
+    console.log(req.session,'<<<<<<');
+    console.log(req.params, 'params<<<<<<<');
     if(req.session.currentUser){
         if (req.session.currentUser.role === "customer") {
-            next()
+            if(req.params.idUser == req.session.currentUser.id){
+                next()
+            }
+            else {
+                res.redirect(`/users/${req.session.currentUser.id}/dashboard`)
+            }
         } else {
             res.redirect('/')
         }
@@ -10,3 +17,6 @@ module.exports = (req, res, next) => {
         res.redirect('/')
     }
 }
+
+// s%3AgPq61gwXFIb_ahJ2QNcdArXbfvNvjvVw.gmdpAcUc9keuAaWJPIoFCuuHmYCxkUpsL5nBkvVC4WY
+// s%3AIvI-yzDVnHMbPaFG-AC0lF8KZb2s0ZuY.frR%2Fai70sWjcZ2NlptX0ybQ2hBs5eLsBVt%2FmnM1p1SY
--- routes/routerUser.js
+++ routes/routerUser.js
@@ -21,24 +21,25 @@ router.post('/register', isNotLogin, UserController.Create)
 router.get('/admin/edit-customer', isAdmin, UserController.findAllCustomer)
 
 
-router.post('/register', UserController.Create)
-router.get('/admin/edit-customer', UserController.findAllCustomer)
-router.get('/:idUser/dashboard', ItemController.findAll)
+router.post('/register', isNotLogin, UserController.Create)
+router.get('/admin/edit-customer', isAdmin, UserController.findAllCustomer)
+
+router.get('/:idUser/dashboard', isCustomer,ItemController.findAll)
 
 
 // BUAT DELETE
 router.get('/admin/edit-customer/delete/:id', isAdmin, UserController.delete)
 
 // UNTUK EDIT CUSTOMER
-router.get('/admin/edit-customer/:id', UserController.findByPk)
-router.post('/admin/edit-customer/:id', UserController.update)
+router.get('/admin/edit-customer/:id', isAdmin, UserController.findByPk)
+router.post('/admin/edit-customer/:id',isAdmin, UserController.update)
 
-router.get('/:idUser/dashboard/filterItem/:field/:value', ItemController.findByFilter)
-router.get('/:idUser/dashboard/findName/:name', ItemController.filterByName)
-router.get('/:idUser/dashboard/findName/:name', ItemController.filterByName)
-router.post('/:idUser/items/:idItem', TransactionController.create)
-router.get('/:idUser/items/:idItem', ItemController.findOne)
-router.get('/:idUser/checkout', UserController.findUserTRX)
+router.get('/:idUser/dashboard/filterItem/:field/:value', isCustomer,ItemController.findByFilter)
+router.get('/:idUser/dashboard/findName/:name', isCustomer,ItemController.filterByName)
+router.get('/:idUser/dashboard/findName/:name', isCustomer,ItemController.filterByName)
+router.post('/:idUser/items/:idItem', isCustomer,TransactionController.create)
+router.get('/:idUser/items/:idItem', isCustomer,ItemController.findOne)
+router.get('/:idUser/checkout', isCustomer,UserController.findUserTRX)
     // router.get('/items/:idItem', ItemController.findOne)
 
 // router.get('/items/:idItem', ItemController.findOne)
