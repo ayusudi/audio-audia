629ef58acd57c63c061395cd1d2628712dff99ac
Ayu Sudi <ayusudi.abc@gmail.com>
2019-08-29 16:46:49 +0700

    oke

--- middlewares/isCustomer.js
+++ middlewares/isCustomer.js
@@ -17,6 +17,3 @@ module.exports = (req, res, next) => {
         res.redirect('/')
     }
 }
-
-// s%3AgPq61gwXFIb_ahJ2QNcdArXbfvNvjvVw.gmdpAcUc9keuAaWJPIoFCuuHmYCxkUpsL5nBkvVC4WY
-// s%3AIvI-yzDVnHMbPaFG-AC0lF8KZb2s0ZuY.frR%2Fai70sWjcZ2NlptX0ybQ2hBs5eLsBVt%2FmnM1p1SY
